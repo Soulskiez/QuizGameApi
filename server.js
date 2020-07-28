2eb5bba56e93e496f7aec6c0eeaa53ba51742713
Soulskiez <tylersolnikowski@gmail.com>
2020-07-27 21:38:42 -0400

    latest

--- server.js
+++ server.js
@@ -2,29 +2,29 @@ const express = require('express');
 const app = express();
 const WebSocket = require('ws');
 
-const socketServer = new WebSocket.Server({port: 3030});
-const messages = ['Start Chatting!'];
+const socketServer = new WebSocket.Server({ port: 3030 });
+const messages = [ 'Start Chatting!' ];
 socketServer.on('connection', (socketClient) => {
-  console.log('connected');
-  console.log('Number of clients: ', socketServer.clients.size);
-  socketClient.send(JSON.stringify(messages));
-  
-  socketClient.on('message', (message) => {
-    messages.push(message);
-    socketServer.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify([message]));
-      }
-    });
-  });
+	console.log('connected');
+	console.log('Number of clients: ', socketServer.clients.size);
+	socketClient.send(JSON.stringify(messages));
 
-    socketClient.on('close', (socketClient) => {
-        console.log('closed');
-        console.log('Number of clients: ', socketServer.clients.size);
-    });
+	socketClient.on('message', (message) => {
+		messages.push(message);
+		socketServer.clients.forEach((client) => {
+			if (client.readyState === WebSocket.OPEN) {
+				client.send(JSON.stringify([ message ]));
+			}
+		});
+	});
+
+	socketClient.on('close', (socketClient) => {
+		console.log('closed');
+		console.log('Number of clients: ', socketServer.clients.size);
+	});
 });
 
 const port = 8765;
 app.listen(port, () => {
-    console.log(`listening http://localhost:${port}`);
-}); 
+	console.log(`listening http://localhost:${port}`);
+});
